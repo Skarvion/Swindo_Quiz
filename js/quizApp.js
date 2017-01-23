22fa594e9144f17393898740e5422e0b7d1c2586
wgermaine <gerbermein@gmail.com>
2017-01-23 15:29:11 +0700

    added typedAns ctrl, index.html, typedAns template

---
+++ js/quizApp.js
@@ -0,0 +1,20 @@
+var app = angular.module("quizApp");
+
+app.controller("quizCtrl"), function($scope,http) {
+	var rawdata = "";
+
+	$scope.readData = function() {
+		
+	};
+	
+	$scope.nextQuestion = function() {
+		
+	}
+	$scope.ShowResultBtn = function () {
+		//Display Button
+	}
+	
+	$scope.ShowAnsDescDialog = function () {
+		
+	}
+});
