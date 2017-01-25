eb5d65fd54e7add5000b6606ff1db57b0a2ab092
LMDyer <100593490@student.swin.edu.au>
2017-01-25 18:51:23 +0700

    Added about section and adjusted for smaller screens

--- js/validate.js
+++ js/validate.js
@@ -1,19 +1,18 @@
-var typingTries = 0
+var typingTries = 1
 function validateTyping() {
 	var answer = document.getElementById("answer");
 	var check = document.getElementById("typingCheck");
-	var tries = 0;
 	if (answer.value == "Australia") {
-		answer.style.backgroundColor = "green";
+		answer.style.backgroundColor = "#90EE90";
 		alert("right");
 		answer.disabled = true;
 		check.disabled = true;
 	}
 	else {
-		if (typingTries < 3) {
-			answer.style.backgroundColor = "red";
+		if (typingTries <= 3) {
+			answer.style.backgroundColor = "#FA8072";
+			alert("wrong, attempt " + typingTries + " of 3");
 			typingTries++;
-			alert("wrong, tries left: " + typingTries);
 		}
 		else {
 			answer.disabled = true;
@@ -22,6 +21,24 @@ function validateTyping() {
 	}
 }
 
-function validateMulti() {
+function validateMulti(selection) {
+	var choice = document.getElementById(selection);
+	alert(selection.innerHTML);
+	if(document.getElementById(selection).childNodes[0].nodeValue == "text") {
+		selection.style.backgroundColor = "#90EE90";
+		alert("right");
+	}
+	else {
+		selection.style.backgroundColor = "#FA8072";
+		alert("wrong, the answer was Koala");
+	}
+	document.getElementById("multi1").disabled = true;
+	document.getElementById("multi2").disabled = true;
+	document.getElementById("multi3").disabled = true;
+	document.getElementById("multi4").disabled = true;
+}
 
+function hideMe(ID) {
+	var checkButton = document.getElementById(ID);
+	checkButton.style.display = "none";
 }
